0ad8edd9cb520477a2c90838cdf78572803110c8
papasmf1 <papasmf1@gmail.com>
2024-07-23 09:25:41 +0900

    함수 연습

--- DemoLoop.py
+++ DemoLoop.py
@@ -9,4 +9,16 @@ lst = ["apple", 100, 3.14]
 for item in lst:
     print(item)
 
-    
+print("---range()함수---")
+print(list(range(1,11)))
+print(list(range(2000, 2025)))
+print(list(range(1,32)))
+
+print("---리스트 내장---")
+lst = list(range(1,11))
+print( [i**2 for i in lst if i>5] )
+tp = ("apple","orange")
+print( [len(i) for i in tp] )
+
+
+
