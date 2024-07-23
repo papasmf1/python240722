8003770a2624da87238fff6c1b0e05aa50ea92bf
papasmf1 <papasmf1@gmail.com>
2024-07-23 09:38:25 +0900

    함수 연습 완료

---
+++ DemoFilter.py
@@ -0,0 +1,19 @@
+# DemoFilter.py 
+lst = [10,25,30]
+iterL = filter(None, lst)
+for item in iterL:
+    print("item:{0}".format(item))
+
+#함수 정의
+def getBiggerThan20(i):
+    return i > 20
+
+print("---필터링함수사용---")
+iterL = filter(getBiggerThan20, lst)
+for item in iterL:
+    print("item:{0}".format(item))
+
+print("---람다함수사용---")
+iterL = filter(lambda x:x>20, lst)
+for item in iterL:
+    print("item:{0}".format(item))
